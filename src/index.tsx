afc3d64a8c2fa9a67216c965653b9aabd2a67f28
sungjoon111 <sungjoon25@yonsei.ac.kr>
2025-04-19 05:06:46 +0900

    refactor

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,9 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import "./index.css";
-import App from "./renderer/App";
+
+import "@/index.css";
+
+import App from "@renderer/App";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
